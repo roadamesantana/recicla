434fae59f6f9a0818b8a45033651f0594a78b2b3
Ronald Sant'Ana <you@example.com>
2019-09-24 15:20:33 +0100

    Adição de calculo de quantidade e peso total para um determinado bairro. Se o mesmo não for informado, faz o cálculo com todos os registros encontrados no período.

--- src/main/java/com/recila/reader/Application.java
+++ src/main/java/com/recila/reader/Application.java
@@ -1,8 +1,11 @@
 package com.recila.reader;
 
 import java.io.File;
-import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
@@ -24,18 +27,93 @@ public class Application {
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-			ObjectMapper mapper = new ObjectMapper();
-			TypeReference<Collection<ReciclaDTO>> typeReference = new TypeReference<Collection<ReciclaDTO>>() {};
-
-			File file = new File("/home/ronald/workspace/spring-recicla/recila.reader/recicla.json");
-
 			try {
+				ObjectMapper mapper = new ObjectMapper();
+				TypeReference<Collection<ReciclaDTO>> typeReference = new TypeReference<Collection<ReciclaDTO>>() {};
+				
+				File file = new File("/home/ronald/workspace/spring-recicla/recila.reader/recicla.json");
 				List<ReciclaDTO> list = mapper.readValue(file, typeReference);
-				list.stream().forEach(x -> System.out.println(x.toString()));
-				System.out.println("FIM");
-			} catch (IOException e) {
+				
+				Date inicio = stringToDate("2019-08-14 00:00:00");
+				Date fim = stringToDate("2019-08-15 00:00:00");
+				String bairro = "";
+				
+				final List<ReciclaDTO> filtered = getDataBetween(list, inicio, fim, bairro);
+				
+				Integer pesoTotal = getPesoTotal(filtered);
+				Integer qtdTotal = getQtdTotal(filtered);
+				
+				SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");  
+				
+				String msg = "No período entre " + formatter.format(inicio) + " e " + formatter.format(fim) + ", ";
+				if (bairro != null && !bairro.isEmpty()) {
+					msg += "no bairro " + bairro + ", ";
+				} 
+				
+				msg += "no peso total foi " + pesoTotal + ", e a quantidade total foi " + qtdTotal;
+				
+				System.out.println(msg);
+				
+			} catch (Exception e) {
 				System.out.println("Erro: " + e);
 			}
 		};
+	}
+	
+	private Integer getPesoTotal(List<ReciclaDTO> list) {
+		Integer pesoTotal = 0;
+		
+		for (ReciclaDTO dto : list) {
+			pesoTotal += dto.getPeso();
+		}
+		
+		return pesoTotal;
+	}
+	
+	private Integer getQtdTotal(List<ReciclaDTO> list) {
+		Integer qtdTotal = 0;
+		
+		for (ReciclaDTO dto : list) {
+			qtdTotal += dto.getQuantidade();
+		}
+		
+		return qtdTotal;
+	}
+	
+	private List<ReciclaDTO> getDataBetween(List<ReciclaDTO> list, Date inicio, Date fim, String bairro) {
+		List<ReciclaDTO> auxList = new ArrayList<>();
+		
+		for(ReciclaDTO dto : list) {
+			Date data = stringToDate(dto.getCarimboDeDatahora());
+			
+			// Valida se está entre as datas
+			if(data.after(inicio) && data.before(fim)) {
+				
+				// Valida se não passou nenhum bairro, ignora e adiciona de qualquer bairro
+				if (bairro == null || bairro.isEmpty()) {
+					auxList.add(dto);					
+				} else {
+					
+					// Se passou bairro, somente adicionar os bairro correspondentes
+					if (bairro.equals(dto.getBairro())) {
+						auxList.add(dto);
+					}
+				}
+			}
+		}
+		
+		return auxList;
+	}
+
+	private Date stringToDate(String strDate) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		try {
+			return formatter.parse(strDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
